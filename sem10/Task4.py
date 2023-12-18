d1b26aa2f7aae42e82186d71d78a8fab341eb053
Mariya <Maryblinova@mail.ru>
2023-12-19 02:45:54 +0300

    Homework10

---
+++ sem10/Task4.py
@@ -0,0 +1,14 @@
+# Преобразовать слова «разработка», «администрирование», «protocol»,
+# «standard» из строкового представления в байтовое и выполнить
+# обратное преобразование (используя методы encode и decode).
+#
+# Подсказки:
+# --- используйте списки и циклы, не дублируйте функции
+
+my_list = ['разработка', 'администрирование', 'protocol', 'standard']
+for line in my_list:
+    var_bytes = line.encode('utf-8')
+    print(var_bytes)
+    var_srt = bytes.decode(var_bytes, 'utf-8')
+    print(var_srt)
+    print()
